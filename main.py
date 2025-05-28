4fb5af3fc4bff91ca11ef7631f5ba3bf9de7d43d
Dmitry-Shved <dimasved009@gmail.com>
2025-05-29 01:07:16 +0300

    create start commsnd

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-def main():
-    pass
+import logging
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+
+TOKEN = '7813563323:AAFAz5xbH1fElCEIvfRRHH9R3_qs1BdVw2M'
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}, хочешь узнать насколькы ты сегодня nicita55?",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
 if __name__ == '__main__':
     main()
